9f25d59716bfee3f1a36b250d7ac304735915006
Rathanon <rathanon.sin@student.mahidol.edu>
2021-07-14 13:23:08 +0700

    test file to add to table

--- src/main/java/io/muic/ooc/webapp/security/TestDatabaseConnection.java
+++ src/main/java/io/muic/ooc/webapp/security/TestDatabaseConnection.java
@@ -3,6 +3,7 @@ package io.muic.ooc.webapp.security;
 import com.zaxxer.hikari.HikariDataSource;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class TestDatabaseConnection {
@@ -11,15 +12,26 @@ public class TestDatabaseConnection {
         ds.setMaximumPoolSize(20);
         ds.setDriverClassName("org.mariadb.jdbc.Driver");
         ds.setJdbcUrl("jdbc:mariadb://localhost:3306/login_webapp");
-        ds.addDataSourceProperty("user", "root");
-        ds.addDataSourceProperty("password", "mansahej");
+        ds.addDataSourceProperty("user", "mansahej");
+        ds.addDataSourceProperty("password", "mansahej20");
         ds.setAutoCommit(false);
 
 
         try {
             Connection connection = ds.getConnection();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+            String sql = "INSERT INTO tbl_user(username, password, display_name) VALUES (?, ?, ?);";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            // setting username column
+            ps.setString(1, "my_username");
+            // setting password column
+            ps.setString(2, "my_password");
+            // setting display name column
+            ps.setString(3, "my_display_name");
+            ps.executeUpdate();
+            //manually commit the change
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
 
     }
