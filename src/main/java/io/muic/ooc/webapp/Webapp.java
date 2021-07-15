d75568e56bb2126ec18dd33f6bc96251ded7502b
Rathanon <rathanon.sin@student.mahidol.edu>
2021-07-15 19:01:58 +0700

    Added customized error page and redirection

--- src/main/java/io/muic/ooc/webapp/Webapp.java
+++ src/main/java/io/muic/ooc/webapp/Webapp.java
@@ -9,6 +9,7 @@ import io.muic.ooc.webapp.service.UserService;
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.tomcat.util.descriptor.web.ErrorPage;
 
 public class Webapp {
 
@@ -30,6 +31,12 @@ public class Webapp {
         try {
             ctx = tomcat.addWebapp("", docBase.getAbsolutePath());
             servletRouter.init(ctx);
+
+            // customize error page
+            ErrorPage error404Page = new ErrorPage();
+            error404Page.setErrorCode(404);
+            error404Page.setLocation("/WEB-INF/error404.jsp");
+            ctx.addErrorPage(error404Page);
 
             tomcat.start();
             tomcat.getServer().await();
